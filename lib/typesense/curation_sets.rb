2d9c94a94f4c7e284757fd94c9b22fc24f8c70e8
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-09-23 11:50:56 +0300

    feat(curation): add curation set classes

---
+++ lib/typesense/curation_sets.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Typesense
+  class CurationSets
+    RESOURCE_PATH = '/curation_sets'
+
+    def initialize(api_call)
+      @api_call = api_call
+    end
+
+    def upsert(curation_set_name, curation_set_data)
+      @api_call.put("#{self.class::RESOURCE_PATH}/#{URI.encode_www_form_component(curation_set_name)}", curation_set_data)
+    end
+
+    def retrieve
+      @api_call.get(self.class::RESOURCE_PATH)
+    end
+
+    def [](curation_set_name)
+      CurationSet.new(curation_set_name, @api_call)
+    end
+
+    def respond_to_missing?(_method_name, _include_private = false)
+      true
+    end
+
+    def method_missing(method_name, *_args, &_block)
+      self[method_name.to_s]
+    end
+  end
+end
