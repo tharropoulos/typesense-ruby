fe16b8d8b1c649631b4f7a48168e64ecf52659e2
Fanis Tharropoulos <ftharropoulos@gmail.com>
2026-06-10 14:46:33 +0300

    docs(aliases): add yardoc for alias class methods

--- lib/typesense/alias.rb
+++ lib/typesense/alias.rb
@@ -7,10 +7,22 @@ module Typesense
       @api_call = api_call
     end
 
+    # Find out which collection an alias points to by fetching it
+    #
+    # @example
+    #   client.aliases['my-alias'].retrieve
+    #
+    # @see https://typesense.org/docs/latest/api/collection-alias.html#retrieve-an-alias
     def retrieve
       @api_call.get(endpoint_path)
     end
 
+    # Delete an alias
+    #
+    # @example
+    #   client.aliases['my-alias'].delete
+    #
+    # @see https://typesense.org/docs/latest/api/collection-alias.html#delete-an-alias
     def delete
       @api_call.delete(endpoint_path)
     end
--- lib/typesense/aliases.rb
+++ lib/typesense/aliases.rb
@@ -9,14 +9,34 @@ module Typesense
       @aliases = {}
     end
 
+    # Create or update a collection alias.
+    #
+    # @example
+    #   client.aliases.upsert('my-alias', 'collection_name' => 'products')
+    #
+    # @see https://typesense.org/docs/latest/api/collection-alias.html#create-or-update-an-alias
     def upsert(alias_name, mapping)
       @api_call.put(endpoint_path(alias_name), mapping)
     end
 
+    # List all aliases and the corresponding collections that they map to.
+    #
+    # @example
+    #   client.aliases.retrieve
+    #
+    # @see https://typesense.org/docs/latest/api/collection-alias.html#list-all-aliases
     def retrieve
       @api_call.get(RESOURCE_PATH)
     end
 
+    # Access an individual collection alias by name.
+    #
+    # @example
+    #   client.aliases['my-alias'].retrieve
+    #
+    # @see https://typesense.org/docs/latest/api/collection-alias.html
+    #
+    # @return [Alias]
     def [](alias_name)
       @aliases[alias_name] ||= Alias.new(alias_name, @api_call)
     end
