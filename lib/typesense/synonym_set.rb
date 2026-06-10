1a35e4248df7243c7bd09a6723a1023c6db110e2
Fanis Tharropoulos <ftharropoulos@gmail.com>
2026-06-10 14:50:30 +0300

    docs(synonym-sets): add yardoc for synonym set class methods

--- lib/typesense/synonym_set.rb
+++ lib/typesense/synonym_set.rb
@@ -7,14 +7,32 @@ module Typesense
       @api_call = api_call
     end
 
+    # Create or update a synonym set with the given name
+    #
+    # @example
+    #   client.synonym_sets['my-set'].upsert('items' => [{ 'id' => 'syn-1', 'synonyms' => ['nyc', 'new york'] }])
+    #
+    # @see https://typesense.org/docs/latest/api/synonyms.html
     def upsert(params)
       @api_call.put(endpoint_path, params)
     end
 
+    # Retrieve a specific synonym set by its name
+    #
+    # @example
+    #   client.synonym_sets['my-set'].retrieve
+    #
+    # @see https://typesense.org/docs/latest/api/synonyms.html
     def retrieve
       @api_call.get(endpoint_path)
     end
 
+    # Delete a specific synonym set by its name
+    #
+    # @example
+    #   client.synonym_sets['my-set'].delete
+    #
+    # @see https://typesense.org/docs/latest/api/synonyms.html
     def delete
       @api_call.delete(endpoint_path)
     end
--- lib/typesense/synonym_sets.rb
+++ lib/typesense/synonym_sets.rb
@@ -9,14 +9,34 @@ module Typesense
       @synonym_sets = {}
     end
 
+    # Create or update a synonym set with the given name
+    #
+    # @example
+    #   client.synonym_sets.upsert('my-set', 'items' => [{ 'id' => 'syn-1', 'synonyms' => ['nyc', 'new york'] }])
+    #
+    # @see https://typesense.org/docs/latest/api/synonyms.html
     def upsert(synonym_set_name, params)
       @api_call.put(endpoint_path(synonym_set_name), params)
     end
 
+    # Retrieve all synonym sets
+    #
+    # @example
+    #   client.synonym_sets.retrieve
+    #
+    # @see https://typesense.org/docs/latest/api/synonyms.html
     def retrieve
       @api_call.get(endpoint_path)
     end
 
+    # Access an individual synonym set by name.
+    #
+    # @example
+    #   client.synonym_sets['my-set'].retrieve
+    #
+    # @see https://typesense.org/docs/latest/api/synonyms.html
+    #
+    # @return [SynonymSet]
     def [](synonym_set_name)
       @synonym_sets[synonym_set_name] ||= SynonymSet.new(synonym_set_name, @api_call)
     end
