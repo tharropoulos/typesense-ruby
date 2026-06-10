f7aea92e4284c52b8deb01b19c6e9fc17aa24a6c
Fanis Tharropoulos <ftharropoulos@gmail.com>
2026-06-10 14:38:46 +0300

    docs(analytics): add yardoc for analytics class methods

--- lib/typesense/analytics.rb
+++ lib/typesense/analytics.rb
@@ -8,10 +8,29 @@ module Typesense
       @api_call = api_call
     end
 
+    # Access the analytics rules resource. Use it to list or create rules, or as an array
+    # to access a single rule by ID.
+    #
+    # @example
+    #   client.analytics.rules.retrieve
+    # @example
+    #   client.analytics.rules['rule-1'].retrieve
+    #
+    # @see https://typesense.org/docs/latest/api/analytics-query-suggestions.html
+    #
+    # @return [AnalyticsRules]
     def rules
       @rules ||= AnalyticsRules.new(@api_call)
     end
 
+    # Access the analytics events resource to send analytics events.
+    #
+    # @example
+    #   client.analytics.events.create('type' => 'click', 'name' => 'products_click', 'data' => {})
+    #
+    # @see https://typesense.org/docs/latest/api/analytics-query-suggestions.html
+    #
+    # @return [AnalyticsEvents]
     def events
       @events ||= AnalyticsEvents.new(@api_call)
     end
