5d270993e4ee8a9353db64310a1c269ec65d39b3
Fanis Tharropoulos <ftharropoulos@gmail.com>
2026-06-10 14:48:31 +0300

    docs(keys): add yardoc for api key class methods

--- lib/typesense/key.rb
+++ lib/typesense/key.rb
@@ -7,10 +7,22 @@ module Typesense
       @api_call = api_call
     end
 
+    # Retrieve (metadata about) a key. Only the key prefix is returned when you retrieve a key. Due to security reasons, only the create endpoint returns the full API key.
+    #
+    # @example
+    #   client.keys[1].retrieve
+    #
+    # @see https://typesense.org/docs/latest/api/api-keys.html#retrieve-an-api-key
     def retrieve
       @api_call.get(endpoint_path)
     end
 
+    # Delete an API key given its ID.
+    #
+    # @example
+    #   client.keys[1].delete
+    #
+    # @see https://typesense.org/docs/latest/api/api-keys.html#delete-api-key
     def delete
       @api_call.delete(endpoint_path)
     end
--- lib/typesense/keys.rb
+++ lib/typesense/keys.rb
@@ -13,14 +13,32 @@ module Typesense
       @keys = {}
     end
 
+    # Create an API Key with fine-grain access control. You can restrict access on both a per-collection and per-action level. The generated key is returned only during creation. You want to store this key carefully in a secure place.
+    #
+    # @example
+    #   client.keys.create('description' => 'Search-only key', 'actions' => ['documents:search'], 'collections' => ['*'])
+    #
+    # @see https://typesense.org/docs/latest/api/api-keys.html#create-an-api-key
     def create(parameters)
       @api_call.post(RESOURCE_PATH, parameters)
     end
 
+    # Retrieve (metadata about) all keys.
+    #
+    # @example
+    #   client.keys.retrieve
+    #
+    # @see https://typesense.org/docs/latest/api/api-keys.html#list-all-keys
     def retrieve
       @api_call.get(RESOURCE_PATH)
     end
 
+    # Generate a scoped search-only API key with embedded parameters such as `filter_by` or `expires_at`.
+    #
+    # @example
+    #   client.keys.generate_scoped_search_key('search-only-key', 'filter_by' => 'company_id:124', 'expires_at' => 1700000000)
+    #
+    # @see https://typesense.org/docs/latest/api/api-keys.html#generate-scoped-search-key
     def generate_scoped_search_key(search_key, parameters)
       parameters_json = JSON.dump(parameters)
       digest = Base64.encode64(OpenSSL::HMAC.digest('sha256', search_key, parameters_json)).gsub("\n", '')
@@ -29,6 +47,14 @@ module Typesense
       Base64.encode64(raw_scoped_key).gsub("\n", '')
     end
 
+    # Access an individual API key by ID.
+    #
+    # @example
+    #   client.keys[1].retrieve
+    #
+    # @see https://typesense.org/docs/latest/api/api-keys.html
+    #
+    # @return [Key]
     def [](id)
       @keys[id] ||= Key.new(id, @api_call)
     end
