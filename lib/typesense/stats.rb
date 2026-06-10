99a8300e180d528c895a03e6c8a5842c149e6997
Fanis Tharropoulos <ftharropoulos@gmail.com>
2026-06-10 14:36:49 +0300

    docs(stats): add yardoc for stats class methods

--- lib/typesense/stats.rb
+++ lib/typesense/stats.rb
@@ -8,6 +8,12 @@ module Typesense
       @api_call = api_call
     end
 
+    # Get stats about API endpoints.
+    #
+    # @example
+    #   client.stats.retrieve
+    #
+    # @see https://typesense.org/docs/latest/api/cluster-operations.html
     def retrieve
       @api_call.get(RESOURCE_PATH)
     end
