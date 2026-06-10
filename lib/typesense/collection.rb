84a8bf7ced20fb3d9220a3b3506b27d5818fec84
Fanis Tharropoulos <ftharropoulos@gmail.com>
2026-06-10 14:42:20 +0300

    docs(collections): add yardoc for collection class methods

--- lib/typesense/collection.rb
+++ lib/typesense/collection.rb
@@ -2,7 +2,44 @@
 
 module Typesense
   class Collection
-    attr_reader :documents, :overrides, :synonyms
+    # Access the documents resource for this collection. Use it to list, index, search, import, or export documents,
+    # or as an array to access a single document by ID.
+    #
+    # @example
+    #   client.collections['products'].documents.create('id' => '1', 'title' => 'Hat')
+    # @example
+    #   client.collections['products'].documents['1'].retrieve
+    #
+    # @see https://typesense.org/docs/latest/api/documents.html
+    #
+    # @return [Documents]
+    attr_reader :documents
+
+    # Access the legacy overrides (curation) resource for this collection. Use it to list or upsert overrides,
+    # or as an array to access a single override by ID.
+    #
+    # @example
+    #   client.collections['products'].overrides.upsert('promote-hat', 'rule' => { 'query' => 'hat', 'match' => 'exact' }, 'includes' => [])
+    # @example
+    #   client.collections['products'].overrides['promote-hat'].retrieve
+    #
+    # @see https://typesense.org/docs/latest/api/curation.html
+    #
+    # @return [Overrides]
+    attr_reader :overrides
+
+    # Access the legacy synonyms resource for this collection. Use it to list or upsert synonyms,
+    # or as an array to access a single synonym by ID.
+    #
+    # @example
+    #   client.collections['products'].synonyms.upsert('syn-1', 'synonyms' => ['nyc', 'new york'])
+    # @example
+    #   client.collections['products'].synonyms['syn-1'].retrieve
+    #
+    # @see https://typesense.org/docs/latest/api/synonyms.html
+    #
+    # @return [Synonyms]
+    attr_reader :synonyms
 
     def initialize(name, api_call)
       @name      = name
@@ -12,14 +49,32 @@ module Typesense
       @synonyms  = Synonyms.new(@name, @api_call)
     end
 
+    # Retrieve the details of a collection, given its name.
+    #
+    # @example
+    #   client.collections['products'].retrieve
+    #
+    # @see https://typesense.org/docs/latest/api/collections.html#retrieve-a-collection
     def retrieve
       @api_call.get(endpoint_path)
     end
 
+    # Update a collection's schema to modify the fields and their types.
+    #
+    # @example
+    #   client.collections['products'].update('fields' => [{ 'name' => 'tags', 'type' => 'string[]' }])
+    #
+    # @see https://typesense.org/docs/latest/api/collections.html#update-or-alter-a-collection
     def update(update_schema)
       @api_call.patch(endpoint_path, update_schema)
     end
 
+    # Permanently drops a collection. This action cannot be undone. For large collections, this might have an impact on read latencies.
+    #
+    # @example
+    #   client.collections['products'].delete
+    #
+    # @see https://typesense.org/docs/latest/api/collections.html#drop-a-collection
     def delete
       @api_call.delete(endpoint_path)
     end
--- lib/typesense/collections.rb
+++ lib/typesense/collections.rb
@@ -9,14 +9,34 @@ module Typesense
       @collections = {}
     end
 
+    # When a collection is created, we give it a name and describe the fields that will be indexed from the documents added to the collection.
+    #
+    # @example
+    #   client.collections.create('name' => 'products', 'fields' => [{ 'name' => 'title', 'type' => 'string' }])
+    #
+    # @see https://typesense.org/docs/latest/api/collections.html#create-a-collection
     def create(schema)
       @api_call.post(RESOURCE_PATH, schema)
     end
 
+    # Returns a summary of all your collections. The collections are returned sorted by creation date, with the most recent collections appearing first.
+    #
+    # @example
+    #   client.collections.retrieve
+    #
+    # @see https://typesense.org/docs/latest/api/collections.html#list-all-collections
     def retrieve(options = {})
       @api_call.get(RESOURCE_PATH, options)
     end
 
+    # Access an individual collection by name.
+    #
+    # @example
+    #   client.collections['products'].retrieve
+    #
+    # @see https://typesense.org/docs/latest/api/collections.html
+    #
+    # @return [Collection]
     def [](collection_name)
       @collections[collection_name] ||= Collection.new(collection_name, @api_call)
     end
