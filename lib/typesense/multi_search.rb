a1a0139ff9dbf8665a0583ccc6bc066b6ba09be8
Fanis Tharropoulos <ftharropoulos@gmail.com>
2026-06-10 14:38:17 +0300

    docs(multi-search): add yardoc for multi-search class methods

--- lib/typesense/multi_search.rb
+++ lib/typesense/multi_search.rb
@@ -8,6 +8,14 @@ module Typesense
       @api_call = api_call
     end
 
+    # Send multiple search requests in a single HTTP request. Pass `union: true` to merge results, or omit it to receive a `results` array.
+    #
+    # @example
+    #   client.multi_search.perform('searches' => [{ 'collection' => 'products', 'q' => '*' }])
+    # @example
+    #   client.multi_search.perform('union' => true, 'searches' => [{ 'collection' => 'products', 'q' => '*' }])
+    #
+    # @see https://typesense.org/docs/latest/api/documents.html#federated-multi-search
     def perform(searches, query_params = {})
       @api_call.post(RESOURCE_PATH, searches, query_params)
     end
